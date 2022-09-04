ff9b6ecc63bb9d7d7351fbe42674be15f6dfb52b
Emiliano Modula <emilianomodula@gmail.com>
2022-09-04 19:37:21 -0300

    Ejercicio1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Grupo6TP1
+{
+    class Ejercicio1
+    {
+        static int Suma(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Suma(2, 1));
+        }
+    }
+}
